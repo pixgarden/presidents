74c03a3cbf1f0e1bd86ca7aeb6002f6804b9cb82
François JOLY <francois@gokam.fr>
2019-01-08 12:42:28 +0000

    initial dump

---
+++ pres-popularity.R
@@ -0,0 +1,82 @@
+
+
+
+#path <- file.choose()
+
+setwd("~/Documents/R/presidents")
+
+popularity2 <- read.csv("popularity.csv")
+
+library(tidyverse)
+library(lubridate)
+library(dbplyr)
+
+#as.POSIXct(popularity2$Date, format = "%mmm-%yy")
+
+
+
+#popularity <- dplyr::arrange(popularity2, Date)
+
+
+#popularity2 <- popularity2 %>%
+#  filter(President.of.France != "1974 /1981 V. Giscard d'Estaing")
+
+
+popularity <- popularity2[rev(order(as.Date(popularity2$Date, format="%mmm-%YY"))),]
+
+#library(ggplot2)
+#library(esquisse)
+#esquisse::esquisser()
+
+library(ggplot2)
+library(grid)
+
+p <- ggplot(data = popularity) +
+  aes(x = Month.of.presidency, y = Trust.Rating, color = President.of.France) +
+  geom_line() +
+  theme_minimal() +
+  facet_wrap(vars(President.of.France))+
+  theme(panel.spacing = unit(2, "lines"))+
+  guides(colour = FALSE)+
+  labs(x = "Month of presidency")+
+  labs(y = "Popularity ratings")+
+  labs(title= "\nFrench presidents' popularity over month of presidency\n")+
+  theme(plot.title = element_text(size=25))
+
+#p
+
+gp <- ggplotGrob(p)
+
+library(gtable)
+
+
+#gtable_show_layout(gp)
+
+empty.area <- gtable_filter(gp, "panel", trim = F)
+empty.area <- empty.area$layout[sapply(empty.area$grob,
+                                       function(x){class(x)[[1]]=="zeroGrob"}),]
+
+empty.area$t <- empty.area$t - 1 #extend up by 1 cell to cover facet header
+empty.area$b <- empty.area$b + 1 #extend down by 1 cell to cover x-axis
+
+library(gridExtra)
+
+gp0 <- gtable_add_grob(x = gp,
+                       grobs = tableGrob("In 2000 France presidential term of\n office length switch from 7 to 5 years.\n\nSource: TNS Sofres \nOct 1978 > Dec 2018 \ndataviz:/u/pixgarden",
+                                         theme = ttheme_minimal()),
+                       t = min(empty.area$t), #16 in this case
+                       l = min(empty.area$l), #8
+                       b = max(empty.area$b), #18
+                       r = max(empty.area$r), #12
+                       name = "??")
+
+#grid::grid.refresh(gp0)
+grid::grid.draw(gp0)
+
+
+
+
+#ggplot(data = popularity) +
+#  aes(x = President.of.France, y = Trust.Rating) +
+#  geom_boxplot(fill = '#0c4c8a') +
+#  theme_minimal()
---
+++ pres-popularity_v2.R
@@ -0,0 +1,85 @@
+
+
+
+#path <- file.choose()
+
+setwd("~/Documents/R/presidents")
+
+popularity <- read.csv("popularity_v2.csv")
+
+#install.packages("splitstackshape")
+
+library(tidyverse)
+library(lubridate)
+library(dbplyr)
+#library(splitstackshape)
+
+#as.POSIXct(popularity2$Date, format = "%mmm-%yy")
+
+#popularity <- cSplit(popularity2, 'Person', sep="|", type.convert=FALSE)
+#
+
+#popularity <- dplyr::arrange(popularity2, Date)
+
+
+#popularity2 <- popularity2 %>%
+#  filter(President.of.France != "1974 /1981 V. Giscard d'Estaing")
+
+
+#popularity$Date <- 
+  
+
+  
+popularity$Date <-  as.Date(paste("01-",popularity$Date), format="%d- %b-%y")
+  
+
+#library(ggplot2)
+#library(esquisse)
+#esquisse::esquisser()
+
+library(ggplot2)
+#install.packages("directlabels")
+library(directlabels)
+
+
+popularity$Type_order = factor(popularity$Type, levels=c('Presidents','PM\'s'))
+popularity$Wing = factor(popularity$Wing, levels=c('Left-wing','Right-wing','It\'s complicated'))
+
+ggplot(data = popularity) +
+  aes(x = Date, y = Trust.Rating, color = Wing, fill = Person_1) +
+  geom_line() +
+  theme_minimal() +
+  facet_wrap(vars(Type))+
+  facet_grid(rows = vars(Type))+
+  expand_limits(y=c(15,90))+
+  scale_color_manual(values=c( "#e20040","#000080","#8A2BE2"))+
+  geom_dl(aes(label = Person_2), method = list(dl.trans(y = y+ 1.2), dl.trans(x = x + 1.2), dl.combine("first.bumpup"), cex = 0.8)) +
+  facet_grid(rows = vars(Type_order))+
+  labs(x = "Time")+
+  labs(y = "Popularity ratings")+
+  labs(title= "\nFrench presidents' popularity ratings compare to PM's\n")+
+  labs(color = "Political spectrum")+
+  theme(plot.title = element_text(size=30, face = "bold"))+
+  theme(axis.text = element_text( size = 14 ),
+         #axis.text.x = element_text( size = 20),
+         #axis.title = element_text( size = 14 ),
+         #legend.position="top",
+         # The new stuff
+         strip.text = element_text(size = 20))+
+  theme(plot.margin=unit(c(1,1,1.5,1.2),"cm"))+
+  theme(legend.position=c(0.95,1.08))+
+  theme(legend.background = element_blank())+
+  theme(legend.box.background = element_rect(colour = "grey"))
+  #theme(legend.direction="horizontal")
+
+  #      legend.justification=c(0, 0),
+  #      legend.direction="horizontal")
+  #theme(face = element_text(size=35)
+  #
+#
+#facet_grid(.~Type)
+#geom_dl(aes(label = Person), method = list(dl.combine("first.points", "last.points"), cex = 0.8)) 
+
+#  
+
+
